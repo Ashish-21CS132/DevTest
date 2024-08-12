60304ad689613a0eac3f315a51a5c6e836677794
ashish_21 <ashishguptaji21@gmail.com>
2024-08-12 15:56:35 +0530

    first_commit

---
+++ DevTest1/uploader/views.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from django.shortcuts import render
+from .forms import UploadFileForm
+from django.core.mail import send_mail
+from django.conf import settings
+
+def upload_file(request):
+    success_message = None
+    error_message = None
+    
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                # Process the uploaded file
+                file = request.FILES['file']
+                df = pd.read_excel(file)
+                
+                # Generate summary
+                summary = df.groupby(['Cust State', 'Cust Pin'])['DPD'].count().reset_index()
+                
+                # Send email
+                email_subject = 'Python Assignment - Ashish Gupta'
+                email_body = summary.to_string()
+                send_mail(email_subject, email_body, 'ashu170800@gmail.com', ['roguptaji76@gmail.com'], fail_silently=False)
+                
+                success_message = 'File uploaded and email sent successfully!✌️'
+            except Exception as e:
+                print(e)
+                error_message = 'Failed the process.'
+        else:
+            error_message = 'Invalid file upload.'
+
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'upload.html', {'form': form, 'success_message': success_message, 'error_message': error_message})
